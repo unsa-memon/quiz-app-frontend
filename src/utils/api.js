ffba9f94c984b32e0e677ed15f27185daa3336ed
Unsa Memon <unsamemon121@gmail.com>
2025-06-13 22:01:54 +0500

    add base API utility for quiz app

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: 'http://localhost:5000/api',
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json', 
   },
 })
 
