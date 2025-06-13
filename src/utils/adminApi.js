a94199ffad8bd5561cb09b635f1840c77319bb75
Unsa Memon <unsamemon121@gmail.com>
2025-06-13 22:01:22 +0500

    add utility functions for admin API calls

--- src/utils/adminApi.js
+++ src/utils/adminApi.js
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
-  },
+  }, 
 });
 
 // Add request interceptor to include auth token
